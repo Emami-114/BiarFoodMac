694b00dc299bb23d6d8564e942bf79af84745a32
Emami.114 <emami.114@outlook.de>
2023-08-22 10:17:34 +0200

    Initial Commit

---
+++ BiarFoodMac/BiarFoodMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiarFoodMacApp.swift
+//  BiarFoodMac
+//
+//  Created by Ecc on 22/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BiarFoodMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
